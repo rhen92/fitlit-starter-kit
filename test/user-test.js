9b539b1e4f1e2a437db9958e0e070cc9a04ef851
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-28 17:09:20 -0600

    Finish it block with all the user details

--- test/user-test.js
+++ test/user-test.js
@@ -6,7 +6,7 @@ const User = require('../classes/user');
 describe('User', () => {
   let user;
   beforeEach(() => {
-    user = new User();
+    user = new User({ id: 1, name: 'Zach Johnson', address: '123 Bear Valley Ln, Las Vegas NV 12345-6789', email: 'zachjohnson123@gmail.com', strideLength: 3.5, dailyStepGoal: 10000, friends: []});
   });
 
   it('should be a function', () => {
@@ -16,4 +16,14 @@ describe('User', () => {
   it('should be an instance of User', () => {
     expect(user).to.be.an.instanceof(User);
   });
+
+  it('should instantiate with user details', () => {
+    expect(user.id).to.equal(1);
+    expect(user.name).to.equal('Zach Johnson');
+    expect(user.address).to.equal('123 Bear Valley Ln, Las Vegas NV 12345-6789');
+    expect(user.email).to.equal('zachjohnson123@gmail.com');
+    expect(user.strideLength).to.equal(3.5);
+    expect(user.dailyStepGoal).to.equal(10000);
+    expect(user.friends).to.deep.equal([]);
+  })
 })
