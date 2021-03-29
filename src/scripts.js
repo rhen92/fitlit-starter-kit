b6da515c8a7a42a2cf580590ab805ff007b31149
Zach Johnson <zachjjohns@gmail.com>
2021-03-29 18:06:43 -0500

    Add greetUser() and displayStepGoal() in scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const greeting = document.querySelector('#greeting');
+const todaySteps = document.querySelector('#todaySteps');
+const userStepGoal = document.querySelector('#userStepGoal');
+const averageStepGoal = document.querySelector('#averageStepGoal');
+
+window.addEventListener('load', greetUser);
+// window.addEventListener('load', displaySteps);
+
+function greetUser() {
+  let currentUser = new User(userData[0]);
+  greeting.innerText = `Welcome ${currentUser.getFirstName()}!`;
+  displayStepGoal(currentUser);
+}
+
+function displayStepGoal(currentUser) {
+  let totalUsers = new UserRepository(userData);
+  console.log(totalUsers);
+  userStepGoal.innerText = `Your step goal: ${currentUser.dailyStepGoal}`;
+  averageStepGoal.innerText = `Average goal among users: ${totalUsers.getAverageStepGoal()}`
+}
