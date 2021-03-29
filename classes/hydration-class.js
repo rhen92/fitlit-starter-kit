9fc6691f9f408fa2ea85426c0938b3766c84fac0
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-29 17:47:23 -0600

    Add constructor properties and getNumOunces method

--- classes/hydration-class.js
+++ classes/hydration-class.js
@@ -1,6 +1,12 @@
 class Hydration {
-  constructor() {
+  constructor(userHydrationData) {
+    this.userID = userHydrationData.userID;
+    this.date = userHydrationData.date;
+    this.numOunces = userHydrationData.numOunces;
+  }
 
+  getNumOunces() {
+    return this.numOunces;
   }
 }
 
