2f5211376ad5f6a9056c13db1648ea477aba1d0b
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-28 18:25:04 -0600

    Add getUserData and getAverageStepGoal methods

--- classes/userRepository.js
+++ classes/userRepository.js
@@ -2,6 +2,19 @@ class UserRepository {
   constructor(data) {
     this.users = data;
   }
+
+  getUserData(userId) {
+    return this.users.find(user => user.id === userId);
+  }
+
+  getAverageStepGoal() {
+    const stepGoal = this.users.map(user => user.dailyStepGoal);
+    const totalSteps = stepGoal.reduce((total, step) => {
+      return total + step
+    }, 0);
+    const averageGoal = totalSteps / stepGoal.length;
+    return Math.round(averageGoal);
+  }
 }
 
 if(typeof module !== 'undefined') {
