7e388c9f310a08ca0494b2e3d1df3df27386319d
Zach Johnson <zachjjohns@gmail.com>
2021-04-01 17:43:22 -0500

    Add method for getSleepQualityByWeek

--- src/classes/SleepRepository.js
+++ src/classes/SleepRepository.js
@@ -33,7 +33,6 @@ class SleepRepository {
     return Math.round(avgSleepQuality);
   }
 
-
   getHoursSleptByWeek(id, date) { 
     let day2 = dayjs(date).add(-1, 'day').format('YYYY/MM/DD');
     let day3 = dayjs(day2).add(-1, 'day').format('YYYY/MM/DD');
@@ -50,6 +49,24 @@ class SleepRepository {
       return obj;
     }, {});
     return weekSleep;
+  }
+
+  getSleepQualityByWeek(id, date) {
+    let day2 = dayjs(date).add(-1, 'day').format('YYYY/MM/DD');
+    let day3 = dayjs(day2).add(-1, 'day').format('YYYY/MM/DD');
+    let day4 = dayjs(day3).add(-1, 'day').format('YYYY/MM/DD');
+    let day5 = dayjs(day4).add(-1, 'day').format('YYYY/MM/DD');
+    let day6 = dayjs(day5).add(-1, 'day').format('YYYY/MM/DD');
+    let day7 = dayjs(day6).add(-1, 'day').format('YYYY/MM/DD');
+    let userSleep = this.usersSleep.filter(user => user.userID === id);
+    let weekSleepQuality = userSleep.reduce((obj, sleep) => {
+      if (sleep.date === date || day2 || day3 || day4 || day5 || day6 || day7) {
+        obj[sleep.date] = sleep.sleepQuality;
+      }
+      console.log(obj);
+      return obj;
+    }, {});
+    return weekSleepQuality;
   }
 }
 
