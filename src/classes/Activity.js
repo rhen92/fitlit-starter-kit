c02a7a4b83a18bd42e7d7e398eb67b9a2d26ca5d
Zach Johnson <zachjjohns@gmail.com>
2021-04-02 16:52:01 -0500

    Add first methods for Activity.js

--- src/classes/Activity.js
+++ src/classes/Activity.js
@@ -6,3 +6,12 @@ class Activity {
     this.minutesActive = userActivityData.minutesActive;
     this.flightsOfStairs = userActivityData.flightsOfStairs
   }
+
+  getMilesWalked(stride) {
+    let stepsPerMile = Math.round(5280 / stride);
+    let miles = Math.round(this.numSteps / stepsPerMile);
+    return { [this.date]: miles };
+  }
+  getMinutesActive() {
+    return { [this.date]: this.minutesActive };
+  }
