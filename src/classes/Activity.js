4b91a4160896c9479dfc0073408c76072f5f9c8a
Zach Johnson <zachjjohns@gmail.com>
2021-04-02 16:51:35 -0500

    Create Activity.js

---
+++ src/classes/Activity.js
@@ -0,0 +1,8 @@
+class Activity {
+  constructor(userActivityData) {
+    this.userID = userActivityData.userID;
+    this.date = userActivityData.date;
+    this.numSteps = userActivityData.numSteps;
+    this.minutesActive = userActivityData.minutesActive;
+    this.flightsOfStairs = userActivityData.flightsOfStairs
+  }
