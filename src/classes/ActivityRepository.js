fd2194a9ac4ea2d6b5d4b27088357e05048dc4f1
Rachel Hendricks <rachelhen92@gmail.com>
2021-04-05 16:18:12 -0600

    Add getActivityTotalWeekForUser method

--- src/classes/ActivityRepository.js
+++ src/classes/ActivityRepository.js
@@ -29,13 +29,31 @@ class ActivityRepository {
     let day5 = dayjs(day4).add(-1, 'day').format('YYYY/MM/DD');
     let day6 = dayjs(day5).add(-1, 'day').format('YYYY/MM/DD');
     let day7 = dayjs(day6).add(-1, 'day').format('YYYY/MM/DD');
+    let weekDates = [day, day2, day3, day4, day5, day6, day7];
     let weekActivity = this.findUser(id).reduce((obj, activity) => {
-      if (activity.date === day || day2 || day3 || day4 || day5 || day6 || day7) {
+      if (weekDates.includes(activity.date)) {
         obj += activity.minutesActive;
       }
       return obj;
     }, 0) / 7;
     return Math.round(weekActivity);
+  };
+
+  getActivityTotalWeekForUser(id, day, workout) {
+    let day2 = dayjs(day).add(-1, 'day').format('YYYY/MM/DD');
+    let day3 = dayjs(day2).add(-1, 'day').format('YYYY/MM/DD');
+    let day4 = dayjs(day3).add(-1, 'day').format('YYYY/MM/DD');
+    let day5 = dayjs(day4).add(-1, 'day').format('YYYY/MM/DD');
+    let day6 = dayjs(day5).add(-1, 'day').format('YYYY/MM/DD');
+    let day7 = dayjs(day6).add(-1, 'day').format('YYYY/MM/DD');
+    let weekDates = [day, day2, day3, day4, day5, day6, day7];
+    let weekActivity = this.findUser(id).reduce((obj, activity) => {
+      if (weekDates.includes(activity.date)) {
+        obj[activity.date] = activity[workout];
+      }
+      return obj;
+    }, {});
+    return weekActivity;
   }
 
   getDaysExceededGoal(user) {
